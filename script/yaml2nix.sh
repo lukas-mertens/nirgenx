cd1821cf490caac13c267246eeca7bf20c63fc17
nzbr <mail@nzbr.de>
2021-10-28 06:07:11 +0200

    yaml2nix: use stdin if no file is specified

--- script/yaml2nix.sh
+++ script/yaml2nix.sh
@@ -1,10 +1,6 @@
 #!@bash@/bin/bash
 set -eu
 
-if [ "$1" == "-" ]; then
-  FILE="/dev/stdin"
-else
-  FILE="$1"
-fi
+FILE="${1:--}" # Fallback to stdin (-)
 
-cat $FILE | @yaml2json@/bin/yaml2json | @nixUnstable@/bin/nix-instantiate --eval -E "with builtins; fromJSON (readFile /dev/stdin)" | @nixfmt@/bin/nixfmt
+cat "$FILE" | @yaml2json@/bin/yaml2json | @nixUnstable@/bin/nix-instantiate --eval -E "with builtins; fromJSON (readFile /dev/stdin)" | @nixfmt@/bin/nixfmt
